0381f1ce6929e138f1328e932440162bb26635b5
Leon <Leon4w@outlook.com>
2018-11-27 15:36:06 +0800

    + Extends UIAlertController .

---
+++ LeonExtensions/Classes/UIAlertController+Extension.h
@@ -0,0 +1,19 @@
+//
+//  UIAlertController+UIAlertController.h
+//  LeonExtensions
+//
+//  Created by Leon Tse on 2018/11/27.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIAlertController (Window)
+
+- (void)show;
+
+- (void)show:(BOOL)animated;
+
+@end
+NS_ASSUME_NONNULL_END
